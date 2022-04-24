bcd578e33d32bc605bb82e5a014d4bf1586c1050
apple <1234>
2022-04-24 12:58:06 +0530

    jay

---
+++ app/src/main/java/com/overseastechnologies/GradeApp/activities/LiveClassAdapter.java
@@ -0,0 +1,65 @@
+package com.overseastechnologies.GradeApp.activities;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import GradeApp.R;
+
+public class LiveClassAdapter extends RecyclerView.Adapter<LiveClassAdapter.ViewHolder> {
+
+    LayoutInflater inflater;
+    List<Liveclass> liveClass;
+
+
+    public  LiveClassAdapter(Context ctx ,List<Liveclass> liveClass){
+
+        this.inflater=LayoutInflater.from(ctx);
+        this.liveClass=liveClass;
+
+    }
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=inflater.inflate(R.layout.custom_liveclass_layout,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.subject.setText(liveClass.get(position).getSubjectName());
+   // picasso.get().load
+    }
+
+    @Override
+    public int getItemCount() {
+        return  liveClass.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView subject;
+        ImageView linkcover;
+
+
+
+        public ViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+
+            subject =itemView.findViewById(R.id.subject);
+            linkcover=itemView.findViewById(R.id.link_cover);
+        }
+    }
+}
---
+++ app/src/main/java/com/overseastechnologies/GradeApp/activities/Liveclass.java
@@ -0,0 +1,40 @@
+package com.overseastechnologies.GradeApp.activities;
+
+public class Liveclass {
+            private String  subjectName;
+            private String  coverImage;
+            private String  classUrl;
+
+            public Liveclass(){}
+            public Liveclass(String subjectName,String coverImage,String classUrl){
+
+                this.subjectName= subjectName;
+                this.coverImage=coverImage;
+                this.classUrl=classUrl;
+
+            }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public String getClassUrl() {
+        return classUrl;
+    }
+
+    public void setClassUrl(String classUrl) {
+        this.classUrl = classUrl;
+    }
+}
